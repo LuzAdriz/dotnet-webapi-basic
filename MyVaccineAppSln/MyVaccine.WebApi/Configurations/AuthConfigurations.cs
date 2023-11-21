86b0d96de125339876fc04565bbad199c82e2b75
luzadriz <adrianita9403@gmail.com>
2023-11-21 16:13:50 -0500

    Add refresh token

--- MyVaccineAppSln/MyVaccine.WebApi/Configurations/AuthConfigurations.cs
+++ MyVaccineAppSln/MyVaccine.WebApi/Configurations/AuthConfigurations.cs
@@ -2,7 +2,10 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
+using MyVaccine.WebApi.Literals;
 using MyVaccine.WebApi.Models;
+
+
 namespace MyVaccine.WebApi.Configurations;
 public static class AuthConfigurations
 {
@@ -32,13 +35,14 @@ public static class AuthConfigurations
                 ValidateIssuer = false,
                 ValidateAudience = false,
                 ValidateLifetime = true,
-                ValidateIssuerSigningKey = false,
+                ValidateIssuerSigningKey = true,
                 //ValidIssuer = "tu_issuer",
                 //ValidAudience = "tu_audience",
-                //IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("tu_clave_secreta")),
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable(MyVaccineLiterals.JWT_KEY))),
                 //ClockSkew = TimeSpan.Zero // Evita un desfase de tiempo (opcional)
             };
         });
         return services;
     }
 }
+       
